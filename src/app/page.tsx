cac03df40556c06cc748374feee613bc758d0e63
minomino425 <tomomi.minoda425@gmail.com>
2024-06-02 18:11:38 +0900

    sass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,35 @@
 import Image from "next/image";
 import Link from "next/link";
+import "@/styles/_base.scss";
+import "@/styles/_main.scss";
 
 export default function Home() {
   return (
     <main>
-      <p>
-        <Link href="/">
-          <Image src="/logo.svg" alt="logo" width={153} height={33} />
-        </Link>
-      </p>
+      <div className="main__wrapper">
+        <p className="main__logo">
+          <Link href="/">
+            <Image src="/logo.svg" alt="logo" width={153} height={33} />
+          </Link>
+        </p>
+        <p className="main__copy">
+          <span className="main__copy-01">
+            {Array.from({ length: 7 }, (_, i) => (
+              <img key={i} src={`/copy/${i + 1}.svg`} alt="copy" />
+            ))}
+          </span>
+          <span className="main__copy-02">
+            {Array.from({ length: 8 }, (_, i) => (
+              <img key={i} src={`/copy/${i + 8}.svg`} alt="copy" />
+            ))}
+          </span>
+          <span className="main__copy-03">
+            {Array.from({ length: 8 }, (_, i) => (
+              <img key={i} src={`/copy/${i + 16}.svg`} alt="copy" />
+            ))}
+          </span>
+        </p>
+      </div>
     </main>
   );
 }
