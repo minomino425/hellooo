f22ed8f2b2ed5514341968e10d80e2a8b526f251
minomino <minodatomomi@minodatomominoMacBook-Pro.local>
2024-07-09 19:33:16 +0900

    モーダルのスタイルを調整

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -14,11 +14,16 @@ export default function Modal(props: ModalProps) {
     <>
       {isOpen && (
         <div className="modal">
-          <div className="modal-content">
-            <h2>Chrome拡張機能を追加</h2>
-            <div>
-              <p>
-                <a href="">こちら</a>からChrome拡張機能を追加してください
+          <div className="modal__wrapper">
+            <p className="modal__step">
+              <span className="modal__stepNumber">1</span>
+              <span className="modal__step">2</span>
+              <span className="modal__step">3</span>
+            </p>
+            <p className="modal__title">Chrome拡張機能を追加</p>
+            <div className="modal__content">
+              <p className="modal__text">
+                <a href="" className="modal__link">こちら</a>からChrome拡張機能を追加してください。
               </p>
             </div>
             <button onClick={onClose}>Close</button>
