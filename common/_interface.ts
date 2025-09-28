35fb4b3554a3c798e974eba39c253fe3c6efc381
Koji Kimura <kimura@starryworks.co.jp>
2025-09-28 12:04:33 +0900

    エラー対応

--- common/_interface.ts
+++ common/_interface.ts
@@ -6,6 +6,14 @@ export interface Icon {
 	platform?: 'x' | 'instagram'
 }
 
+export interface IconWithSpriteSheet {
+	account: string
+	url: string
+	dataPath?: string
+	qrPath?: string
+	platform?: 'x' | 'instagram'
+}
+
 export interface LabelTemplate {
 	id: string
 	url: string
---
+++ web/src/app/global.d.ts
@@ -0,0 +1,24 @@
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '@/styles/_base.scss' {
+  const content: void;
+  export default content;
+}
+
+declare module '@/styles/_main.scss' {
+  const content: void;
+  export default content;
+}
+
+declare module '@/styles/_config.scss' {
+  const content: void;
+  export default content;
+}
+
+declare module '@/styles/_modal.scss' {
+  const content: void;
+  export default content;
+}
